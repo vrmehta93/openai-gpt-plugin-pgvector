cbe702dda6acfe323f363c8f8b99659503a5b67c
Denis Magda <dmagda@gridgain.com>
2024-07-15 11:26:47 -0400

    bumped up the Spring AI version and fixed Flyway dependency

--- backend/src/main/java/com/yugaplus/backend/controller/MovieController.java
+++ backend/src/main/java/com/yugaplus/backend/controller/MovieController.java
@@ -16,50 +16,50 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ai.embedding.EmbeddingClient;
+import org.springframework.ai.embedding.EmbeddingModel;
 
 @RestController
 @RequestMapping("/api/movie")
 public class MovieController {
-    private JdbcClient jdbcClient;
+        private JdbcClient jdbcClient;
 
-    private EmbeddingClient aiClient;
+        private EmbeddingModel embeddingModel;
 
-    public MovieController(
-            @Autowired(required = false) EmbeddingClient aiClient,
-            JdbcClient jdbcClient) {
+        public MovieController(
+                        @Autowired(required = false) EmbeddingModel embeddingModel,
+                        JdbcClient jdbcClient) {
 
-        this.aiClient = aiClient;
-        this.jdbcClient = jdbcClient;
-    }
+                this.embeddingModel = embeddingModel;
+                this.jdbcClient = jdbcClient;
+        }
 
-    @GetMapping("/{id}")
-    public MovieResponse getMovieById(@PathVariable Integer id) {
-        Movie movie = jdbcClient.sql(
-                "SELECT * FROM movie WHERE id = ?")
-                .param(id).query(Movie.class).single();
+        @GetMapping("/{id}")
+        public MovieResponse getMovieById(@PathVariable Integer id) {
+                Movie movie = jdbcClient.sql(
+                                "SELECT * FROM movie WHERE id = ?")
+                                .param(id).query(Movie.class).single();
 
-        return new MovieResponse(new Status(true, HttpServletResponse.SC_OK), List.of(movie));
-    }
+                return new MovieResponse(new Status(true, HttpServletResponse.SC_OK), List.of(movie));
+        }
 
-    @GetMapping("/search")
-    public MovieResponse searchMovies(
-            @RequestParam("prompt") String prompt,
-            @RequestParam(name = "rank", required = false) Integer rank,
-            @RequestParam(name = "category", required = false) String category) {
+        @GetMapping("/search")
+        public MovieResponse searchMovies(
+                        @RequestParam("prompt") String prompt,
+                        @RequestParam(name = "rank", required = false) Integer rank,
+                        @RequestParam(name = "category", required = false) String category) {
 
-        List<Double> embedding = aiClient.embed(prompt);
+                List<Double> embedding = embeddingModel.embed(prompt);
 
-        List<Movie> movies = jdbcClient.sql(
-                "SELECT id, title, overview, vote_average, release_date FROM movie"
-                        + " WHERE 1 - (overview_vector <=> :prompt_vector::vector) > 0.7"
-                        + " ORDER BY overview_vector <=> :prompt_vector::vector"
-                        + " LIMIT 3")
-                .param("prompt_vector", embedding.toString())
-                .query(Movie.class).list();
+                List<Movie> movies = jdbcClient.sql(
+                                "SELECT id, title, overview, vote_average, release_date FROM movie"
+                                                + " WHERE 1 - (overview_vector <=> :prompt_vector::vector) > 0.7"
+                                                + " ORDER BY overview_vector <=> :prompt_vector::vector"
+                                                + " LIMIT 3")
+                                .param("prompt_vector", embedding.toString())
+                                .query(Movie.class).list();
 
-        return new MovieResponse(
-                new Status(true, HttpServletResponse.SC_OK),
-                movies);
-    }
+                return new MovieResponse(
+                                new Status(true, HttpServletResponse.SC_OK),
+                                movies);
+        }
 }
